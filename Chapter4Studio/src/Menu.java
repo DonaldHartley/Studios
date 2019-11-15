36e2dcc49b49eea97ef1afa7fc82decab0073941
DonaldHartley <Donald.H.Hartley@gmail.com>
2019-11-14 19:33:05 -0600

    mostly empty studo structure, talked alot with elaine and tyler

---
+++ Chapter4Studio/src/Menu.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Menu {
+  private HashMap<String /*category*/, ArrayList<MenuItems> >allItems;
+  
+  public static void showItems(String category){
+    /*
+    show a specific item from the allItems HashMap
+     */
+    return;
+  }
+  
+}
---
+++ Chapter4Studio/src/MenuItem.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class MenuItem {
+  
+  private String itemName;
+  private String itemCategory;
+  private double price;
+  private LocalDate dateCreated;
+  private String itemDescription;
+  
+  public MenuItem(){
+  }
+}
